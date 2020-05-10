8977668d4e1ee8c2dfe6a3db428adb5c48aa230f
luishco <luiscarvoliveira@gmail.com>
2020-05-10 16:55:13 -0300

    feat: login with firebase

--- pong-mp-client/src/app/auth/login/login.page.ts
+++ pong-mp-client/src/app/auth/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,33 @@ export class LoginPage implements OnInit {
   loginForm: FormGroup
   loginError: string
 
-  constructor(private auth: AuthService, fb: FormBuilder) {
+  constructor(private auth: AuthService, private router: Router, fb: FormBuilder) {
     this.loginForm = fb.group({
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
     });
+  }
+
+  login() {
+    let data = this.loginForm.value;
+
+    if (!data.email) {
+      return;
+    }
+
+    let credentials = {
+      email: data.email,
+      password: data.password
+    };
+
+    this.auth.signInWithEmail(credentials).then(
+      () => this.router.navigateByUrl('/list-rooms'),
+      error => {
+        this.loginError = error.message
+        console.log(error.message)
+      }
+    );
+
   }
 
   ngOnInit() {
