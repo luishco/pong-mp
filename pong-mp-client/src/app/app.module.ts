dc8bcf14e5180ba186cf7d10199da770ff64fe76
luishco <luiscarvoliveira@gmail.com>
2020-06-21 18:19:22 -0300

    feat: ugly new css and added camera

--- pong-mp-client/src/app/app.module.ts
+++ pong-mp-client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireAuth } from 'angularfire2/auth'
 import { firebaseConfig } from '../environments/environment'
 import { AuthService } from './services/auth.service'
 import { SocketIoModule, SocketIoConfig } from 'ng-socket-io'
+import { Camera } from '@ionic-native/camera/ngx'
 
 import { serverAddress } from '../environments/environment'
 
@@ -37,7 +38,8 @@ const socketIoConfig: SocketIoConfig = {
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     AngularFireAuth,
-    AuthService
+    AuthService,
+    Camera
   ],
   bootstrap: [AppComponent]
 })
--- pong-mp-client/src/app/game/list-rooms/list-rooms.page.ts
+++ pong-mp-client/src/app/game/list-rooms/list-rooms.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ɵConsole } from '@angular/core';
 import { Socket } from 'ng-socket-io';
 import { Router } from '@angular/router';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-list-rooms',
@@ -15,7 +16,7 @@ export class ListRoomsPage implements OnInit {
   }
   rooms = {}
 
-  constructor(private socket: Socket, private router: Router) {
+  constructor(private socket: Socket, private router: Router, private camera: Camera) {
     socket.on('update-rooms', (rooms) => {
       this.rooms = rooms
     });
@@ -36,7 +37,7 @@ export class ListRoomsPage implements OnInit {
     room.players[userId] = {
       status: 'waiting'
     }
-    
+
     this.socket.emit('create-room', room);
 
     this.router.navigateByUrl('/room')
@@ -54,6 +55,23 @@ export class ListRoomsPage implements OnInit {
   ionViewWillEnter() {
     this.socket.connect()
     this.socket.emit('set-user-data', this.user)
+  }
+
+  takePicture() {
+    const cameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(cameraOptions).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+    // Handle error
+    });
   }
       
 }
